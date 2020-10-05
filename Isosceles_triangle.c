8478af5fd5b842292049c3ede7ec1b1689e7b742
handsomehsia <71938273+handsomehsia@users.noreply.github.com>
2020-10-05 21:31:51 +0800

    Update Isosceles_triangle.c

--- Isosceles_triangle.c
+++ Isosceles_triangle.c
@@ -2,11 +2,11 @@
 int main()
 {
     int i, j, size = 8;
-    for (i = 1; i <= size; i++)
+    for (i = 1; i < size + 1; i++)
     { // 印出第i列
         for (j = 1; j < size + i; j++)
         { // 每一列有size+i-1個符號
-            if (j <= size - i)
+            if (j < size - i + 1)
             { // 在size-i左邊(含)的符號是空白
                 printf(" ");
             }
